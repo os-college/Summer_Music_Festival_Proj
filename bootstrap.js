facde20e0607b1949218909eb947c78712355a75
os-college <136599275+os-college@users.noreply.github.com>
2023-07-05 19:17:37 +0100

    yeahhhhh

---
+++ bootstrap.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var carousel = document.getElementById('carouselExampleFade');
+    var carouselInstance = new bootstrap.Carousel(carousel, {
+        interval: 3000 
+    });
+});
+  
